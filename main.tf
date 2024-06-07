c099b15508b9e6ce6369ab3c68e68d0210291a7f
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-07 11:19:07 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,56 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1" # Change to your desired region
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create a Subnet
+resource "aws_subnet" "main" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1" # Change to your desired availability zone
+}
+
+# Create a Security Group
+resource "aws_security_group" "instance" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Change to your desired AMI
+  instance_type = "t2.micro"
+
+  subnet_id              = aws_subnet.main.id
+  security_groups        = [aws_security_group.instance.name]
+  key_name               = "Pipeline" # Change to your key pair name
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
