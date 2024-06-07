aceeff3c826b8adad2f05d4b62441242c0f5dfb7
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-07 23:07:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example_a" {
   instance_type = "t2.micro"
   
   subnet_id              = "subnet-0d8c3f22474129496" # Subnet in ap-south-1a
-  security_group_ids     = ["sg-0a40b4e411e819ec1"] # List of security groups
+  vpc_security_group_id     = ["sg-0a40b4e411e819ec1"] # List of security groups
   key_name               = "Pipeline" # Change to your key pair name
 
   tags = {
