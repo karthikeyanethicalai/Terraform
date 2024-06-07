ed0fc2546dfc4a7843ec4cd0c2f22960e1c70ed5
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-07 16:17:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,56 +1,31 @@
-# Configure the AWS Provider
 provider "aws" {
   region = "ap-south-1" # Change to your desired region
 }
 
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-# Create a Subnet
-resource "aws_subnet" "main" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a" # Change to your desired availability zone
-}
-
-# Create a Security Group
-resource "aws_security_group" "instance" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Create an EC2 Instance
-resource "aws_instance" "example" {
+# Create an EC2 Instance in Subnet ap-south-1a
+resource "aws_instance" "example_a" {
   ami           = "ami-0c55b159cbfafe1f0" # Change to your desired AMI
   instance_type = "t2.micro"
-
-  subnet_id              = aws_subnet.main.id
-  security_groups        = [aws_security_group.instance.name]
+  
+  subnet_id              = "subnet-0c74489da56983c03" # Subnet in ap-south-1a
+  security_group_ids     = ["sg-0a40b4e411e819ec1", "sg-060e914c9848f41b8"] # List of security groups
   key_name               = "Pipeline" # Change to your key pair name
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "ExampleInstanceA"
+  }
+}
+
+# Create an EC2 Instance in Subnet ap-south-1c
+resource "aws_instance" "example_c" {
+  ami           = "ami-0c55b159cbfafe1f0" # Change to your desired AMI
+  instance_type = "t2.micro"
+  
+  subnet_id              = "subnet-0d8c3f22474129496" # Subnet in ap-south-1c
+  security_group_ids     = ["sg-0a40b4e411e819ec1", "sg-060e914c9848f41b8"] # List of security groups
+  key_name               = "Pipeline" # Change to your key pair name
+
+  tags = {
+    Name = "ExampleInstanceC"
   }
 }
