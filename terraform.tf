9fc24b107ed4a85daadcb41eab5ea376a949b3c5
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-11 21:54:39 +0530

    terrafrom code

--- terraform.tf
+++ terraform.tf
@@ -1,15 +1,13 @@
-```hcl
+```terraform
 provider "aws" {
   region = "us-west-2"
 }
 
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
+  
   tags = {
-    Name = "my-vpc"
+    Name = "my_vpc"
   }
 }
 ```
