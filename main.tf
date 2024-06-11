6ad1081179908cdec8a768544f18b311ac07d66a
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-11 22:24:32 +0530

    terrafrom code

--- main.tf
+++ main.tf
@@ -1,51 +1,17 @@
-```hcl
+```terraform
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
+resource "aws_instance" "test" {
+  ami           = "ami-0f346136f3b372267"
+  instance_type = "t2.micro"
+  key_name      = "test"
+  subnet_id     = "subnet-09ce403a92a4d5d81"
+  vpc_security_group_ids = ["<your_security_group_id>"]
 
   tags = {
-    Name = "my_vpc"
+    Name = "test"
   }
-}
-
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  tags = {
-    Name = "my_igw"
-  }
-}
-
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-
-  tags = {
-    Name = "my_route_table"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "my_subnet"
-  }
-}
-
-resource "aws_route_table_association" "my_subnet_association" {
-  subnet_id      = aws_subnet.my_subnet.id
-  route_table_id = aws_route_table.my_route_table.id
 }
 ```
