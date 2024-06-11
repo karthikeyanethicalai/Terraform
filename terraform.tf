fdb4fc3b3d8223b0c3110cda4dccaf07fa0018e9
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-11 21:37:59 +0530

    terrafrom code

--- terraform.tf
+++ terraform.tf
@@ -1,49 +1,12 @@
-```terraform
-provider "aws" {
-  region = "us-east-1"
-}
-
+```hcl
+# Define VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
   enable_dns_hostnames = true
-}
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
-}
-
-resource "aws_route_table" "my_route_table" {
-  vpc_id = aws_vpc.my_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.id
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_security_group" "my_security_group" {
-  vpc_id = aws_vpc.my_vpc.id
-  name = "my_security_group"
-  
-  ingress {
-    from_port = 0
-    to_port = 65535
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-  egress {
-    from_port = 0
-    to_port = 65535
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Name = "my_vpc"
   }
 }
 ```
