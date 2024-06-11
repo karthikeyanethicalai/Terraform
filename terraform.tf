360639e0934e15871e48119fd3be6a00a3380dab
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-11 21:40:59 +0530

    terrafrom code

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,31 @@
 ```hcl
-# Define VPC
+# Create a VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
   enable_dns_hostnames = true
+  enable_dns_support = true
+}
 
-  tags = {
-    Name = "my_vpc"
-  }
+# Create an internet gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Attach the internet gateway to the VPC
+resource "aws_vpc_attachment" "my_igw_attach" {
+  vpc_id = aws_vpc.my_vpc.id
+  internet_gateway_id = aws_internet_gateway.my_igw.id
+}
+
+# Create a route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+# Create a route for the internet gateway
+resource "aws_route" "my_route" {
+  route_table_id = aws_route_table.my_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my_igw.id
 }
 ```
