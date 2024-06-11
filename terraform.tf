6e1845ee259c8e952b654cb5e8982ae4946b86e2
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-11 22:12:45 +0530

    terrafrom code

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,41 @@
-```terraform
-provider "aws" {
-  region = "us-west-2"
-}
-
+```hcl
+# Create a VPC
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
-  
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
   tags = {
     Name = "my_vpc"
   }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+
+  tags = {
+    Name = "my_route_table"
+  }
+}
+
+# Associate the route table with the VPC
+resource "aws_vpc_association_route_table" "my_association" {
+  vpc_id = aws_vpc.my_vpc.id
+  route_table_id = aws_route_table.my_route_table.id
 }
 ```
