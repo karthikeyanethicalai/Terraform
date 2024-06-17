f86ee3c7bebe889d69a9cc1b0d2a9c30e83bed7a
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-17 20:29:00 +0530

    terrafrom code

--- main.tf
+++ main.tf
@@ -7,10 +7,12 @@ resource "aws_instance" "test" {
   instance_type = "t2.micro"
   subnet_id     = "subnet-0c74489da56983c03"
   vpc_security_group_ids = ["sg-060e914c9848f41b8"]
-  key_name      = "test"
+  key_name      = "New-Server"
   tags = {
     Name = "test"
   }
-  
-  vpc_security_group_ids = ["sg-060e914c9848f41b8"]
+}
+
+output "public_ip" {
+  value = aws_instance.test.public_ip
 }
