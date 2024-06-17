c984dc550d86f634cdca999b8418690de95c86f4
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-17 19:44:52 +0530

    terrafrom code

--- main.tf
+++ main.tf
@@ -2,21 +2,24 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+  id        = "vpc-002922cb09fef1dc9"
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id     = "vpc-002922cb09fef1dc9"
-  cidr_block = "10.0.0.0/24"
+resource "aws_subnet" "example" {
+  vpc_id     = aws_vpc.example.id
+  cidr_block = "10.0.1.0/24"
+  id        = "subnet-09ce403a92a4d5d81"
 }
 
-resource "aws_instance" "my_instance" {
+resource "aws_instance" "example" {
   ami           = "ami-0f346136f3b372267"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-09ce403a92a4d5d81"
+  subnet_id     = aws_subnet.example.id
   key_name      = "test"
-  vpc_security_group_ids = ["sg-060e914c9848f41b8"]
+  security_groups = ["sg-060e914c9848f41b8"]
+
   tags = {
     Name = "test"
   }
