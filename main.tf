40da474f9c4d05937606ce988b31fa0e5629afc4
karthikeyanethicalai <karthikeyanethicalai@gmail.com>
2024-06-17 20:23:24 +0530

    terrafrom code

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "test" {
   ami           = "ami-0f346136f3b372267"
   instance_type = "t2.micro"
   subnet_id     = "subnet-0c74489da56983c03"
@@ -11,4 +11,6 @@ resource "aws_instance" "example" {
   tags = {
     Name = "test"
   }
+  
+  vpc_security_group_ids = ["sg-060e914c9848f41b8"]
 }
